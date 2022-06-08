9835796b462cc39792a7db301249b66937958244
im6h <danghaiv.18@gmail.com>
2022-06-08 23:18:43 +0700

    feat: add cmd to query quote by anime title

--- cmd/docker_backup.go
+++ cmd/docker_backup.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -12,10 +14,54 @@ var dockerBackupCmd = &cobra.Command{
 	Short: "Command docker backup",
 	Long:  `Command docker backup`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("dockerBackup called")
+		db := args[0]
+		containerId := args[1]
+		dbPassword := args[2]
+		username := args[3]
+		databaseName := args[4]
+		if containerId == "" {
+			log.Fatal("Need container id")
+		}
+		if "" == dbPassword {
+			log.Fatal("Need dbPassword")
+		}
+		if username == "" {
+			log.Fatal("Need username")
+		}
+		if databaseName == "" {
+			log.Fatal("Need databaseName")
+		}
+
+		switch db {
+		case "pg":
+			{
+				backupPg(containerId, dbPassword, username, databaseName)
+				break
+			}
+		case "mysql":
+			{
+				break
+			}
+		default:
+			{
+				log.Fatal("Need database type")
+				break
+			}
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(dockerBackupCmd)
+}
+
+func backupPg(containerId string, dbPassword string, username string, databaseName string) {
+	cmdString := fmt.Sprintf(`exec -i %s /bin/bash -c "PGPASSWORD=%s pg_dump --username %s %s" > /Users/oraichain/Desktop/work/database-sql/testnet/dump_date.sql`, containerId, dbPassword, username, databaseName)
+
+	cmd := exec.Command("docker", cmdString)
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- cmd/quote_anime.go
+++ cmd/quote_anime.go
@@ -21,7 +21,7 @@ var quoteAnimeCmd = &cobra.Command{
 	Short: "Fetch quote anime",
 	Long:  `Use to fetch anime's quote`,
 	Run: func(cmd *cobra.Command, args []string) {
-		quoteAnimeCmdExecute()
+		quoteAnimeCmdExecute(args)
 	},
 }
 
@@ -29,11 +29,9 @@ func init() {
 	rootCmd.AddCommand(quoteAnimeCmd)
 }
 
-func fetchRandomQuote() {
-	var baseURL string = "https://animechan.vercel.app/api"
-	var quote *quote
+func responseData(url string) []byte {
 
-	resp, err := http.Get(baseURL + "/random")
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Panicf("error when fetch random quote: %v\n", err)
 	}
@@ -45,7 +43,33 @@ func fetchRandomQuote() {
 		log.Panicf("error when read data random quote: %v\n", err)
 	}
 
-	err = json.Unmarshal(body, &quote)
+	return body
+}
+
+func fetchQuoteByAnimeName(name string, page string) {
+	var url string = fmt.Sprintf("https://animechan.vercel.app/api/quotes/anime?title=%s&page=%s", name, page)
+	var quotes []*quote
+
+	body := responseData(url)
+
+	err := json.Unmarshal(body, &quotes)
+	if err != nil {
+		log.Panicf("error when unmarshaling data random quote: %v\n", err)
+	}
+
+	for _, quote := range quotes {
+		fmt.Printf("%s by %s\n", quote.Quote, quote.Character)
+	}
+
+}
+
+func fetchRandomQuote() {
+	var url string = "https://animechan.vercel.app/api/random"
+	var quote *quote
+
+	body := responseData(url)
+
+	err := json.Unmarshal(body, &quote)
 	if err != nil {
 		log.Panicf("error when unmarshaling data random quote: %v\n", err)
 	}
@@ -53,6 +77,24 @@ func fetchRandomQuote() {
 	fmt.Printf("%s\n \t %s in %s\n", quote.Quote, quote.Character, quote.Anime)
 }
 
-func quoteAnimeCmdExecute() {
-	fetchRandomQuote()
+func quoteAnimeCmdExecute(args []string) {
+	if len(args) < 1 {
+		fetchRandomQuote()
+		return
+	}
+
+	if len(args) == 1 {
+		title := args[0]
+
+		fetchQuoteByAnimeName(title, "1")
+		return
+	}
+
+	if len(args) > 1 {
+		title := args[0]
+		page := args[1]
+
+		fetchQuoteByAnimeName(title, page)
+		return
+	}
 }
